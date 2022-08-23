db8ea3bcda8d4094d1451b247c0532d1b417e61d
Aiden Lu <alu@stu.naperville203.org>
2022-08-23 12:38:54 -0500

    Started BlueJ
    Chapter 1 notes

---
+++ FirstBlueJProject/chapter1Notes.java
@@ -0,0 +1,40 @@
+
+/**
+ * Aiden
+ * 8/23/22
+ * Notes for Chapter 1
+ * Where we are going to take some code notes
+ * Who helped me: Mr. Hayes, Dad, etc.
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+    
+    /**
+     * main method
+     * Where we are going to code
+     */
+    public static void main(String[] args)
+    {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        //These are the basic data types
+        int x = 9, y, z;
+        double num = 5, num1 = 23.4567, num2;
+        char letter = 'e';
+        boolean choice = false;
+        float anum = 45;
+        long a = 9;
+        short b = 5;
+        
+        //These are objects
+        String word = new String("Aiden");
+        Integer number = new Integer(47);
+        Double number2 = new Double(34.5);
+        
+    }
+}
